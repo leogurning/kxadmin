55a95ba41ad4a20533462e52e098d99d1151743f
leo gurning <leogurning@gmail.com>
2018-04-10 14:34:08 +0700

    adding redis password

--- config.js
+++ config.js
@@ -4,5 +4,6 @@ module.exports = {
     'secret': '!$3cret4@uth',
     'database': 'mongodb://admindb:kaxetadmin@35.200.243.4:27017/kaxetdb',
     'redis_server': '35.200.243.4',
-    'redis_port': 6379
+    'redis_port': 6379,
+    'redis_pass': '$2a$05$o.THiPHg9DsviJrCKkaM.OaEKzJbddJkA1ZqY2jYoZRHzTRjcbdHS-7421694943ef4ab1623b59b3e789c7571aff8'
 }
--- redisconn.js
+++ redisconn.js
@@ -2,6 +2,11 @@ var config = require('./config');
 var redis = require('redis');
 
 var rediscli= redis.createClient(config.redis_port, config.redis_server);
+
+rediscli.auth(config.redis_pass, function (err) {
+    if (err) throw err;
+});
+
 rediscli.on('connect', function() {
     console.log('Connected to Redis...');
 })
